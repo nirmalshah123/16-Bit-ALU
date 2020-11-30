bdb0f5b34af613fa7f5256ac7894d5a8f96ea7f6
Nirmal shah <62292293+nirmalshah123@users.noreply.github.com>
2020-11-30 19:04:30 +0530

    Added zero flag logic

--- ALU_16_bit.vhd
+++ ALU_16_bit.vhd
@@ -3,9 +3,9 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity ALU_16_bit is
-port (A,B 	: in std_logic_vector(15 downto 0);			-- A,B are inputs
+port (A,B 	: in signed(15 downto 0);						-- A,B are inputs
 		sel	: in std_logic_vector(1 downto 0);			-- control pins for MUX
-		op 	: out std_logic_vector(15 downto 0);		-- 16 bit output
+		op 	: buffer signed(15 downto 0);					-- 16 bit output
 		c		: out std_logic;									-- carry
 		z 		: out std_logic);									-- set to 1 if output=0
 end ALU_16_bit;
@@ -22,7 +22,12 @@ begin
 			when "11" 	=> op <= A XOR  B;
 			when others => null;
 			end case;
-				
+		
+		if (op = 0) then											-- setting z flag to 1 if output is 0
+		z <= '1';
+		else
+		z <='0';
+		end if;
 		end process;
 
 end ALU;
