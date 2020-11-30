a580a5f9553efe4790ba9401ce69837f4b07e923
Nirmal shah <62292293+nirmalshah123@users.noreply.github.com>
2020-12-01 01:49:43 +0530

    Added FastAdder

--- ALU_16_bit.vhd
+++ ALU_16_bit.vhd
@@ -4,21 +4,31 @@ use IEEE.numeric_std.all;
 
 entity ALU_16_bit is
 port (A,B 	: in std_logic_vector(15 downto 0);						-- A,B are inputs
-		sel	: in std_logic_vector(1 downto 0);			-- control pins for MUX
-		op 	: buffer std_logic_vector(15 downto 0);					-- 16 bit output
-		z 		: out std_logic);									-- set to 1 if output=0
+		sel	: in std_logic_vector(1 downto 0);						-- control pins for MUX
+		Carry : out std_logic;												-- Carry
+		op 	: buffer std_logic_vector(15 downto 0);				-- 16 bit output
+		z 		: out std_logic);												-- set to 1 if output=0
 end ALU_16_bit;
 
 architecture ALU of ALU_16_bit is
-signal nand_op,xor_op : std_logic_vector(15 downto 0);
+signal nand_op,xor_op,add_op : std_logic_vector(15 downto 0);
+signal carry_op : std_logic;
+	component FastAdder is
+	port(
+	A		:in std_logic_vector(15 downto 0);				--Input A
+	B		:in std_logic_vector(15 downto 0);				--Input B
+	Carry	:out std_logic;										--Final carry
+	op		:out std_logic_vector(15 downto 0));			--Output		
+	end component;
+	
 
-	component NAND16
+	component NAND16 is
 		port(	a 	: in 	std_logic_vector(15 downto 0);
 				b	: in 	std_logic_vector(15 downto 0);
 				o	: out std_logic_vector(15 downto 0));
 	end component;
 
-	component XOR16
+	component XOR16 is
 		port(	a 	: in 	std_logic_vector(15 downto 0);
 				b	: in 	std_logic_vector(15 downto 0);
 				o	: out std_logic_vector(15 downto 0));
@@ -27,11 +37,12 @@ signal nand_op,xor_op : std_logic_vector(15 downto 0);
 begin
 nand_instance : NAND16 port map(a => A, b =>B, o =>nand_op);
 xor_instance  : XOR16  port map(a => A, b =>B, o =>xor_op);
+FastAdder_instance : FastAdder port map(A =>A,B=>B,Carry =>carry_op, op=>add_op);
 	process(A,B,sel,op)
 		begin
 
 			case sel is 
-			when "00" 	=> null;
+			when "00" 	=> op<= add_op;
 			when "01" 	=> null;
 			when "10" 	=> op <= nand_op;
 			when "11" 	=> op <= xor_op;
