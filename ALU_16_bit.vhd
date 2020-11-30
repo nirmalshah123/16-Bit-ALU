3b72d960efea7a3b94bab09d271848f62e7430e1
PURANJAY14 <62548743+PURANJAY14@users.noreply.github.com>
2020-11-30 22:20:00 +0530

    Add files via upload

--- ALU_16_bit.vhd
+++ ALU_16_bit.vhd
@@ -1,33 +1,42 @@
 library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
+use IEEE.STD_LOGIC_1164.ALL;
 
 entity ALU_16_bit is
-port (A,B 	: in signed(15 downto 0);						-- A,B are inputs
-		sel	: in std_logic_vector(1 downto 0);			-- control pins for MUX
-		op 	: buffer signed(15 downto 0);					-- 16 bit output
-		c		: out std_logic;									-- carry
-		z 		: out std_logic);									-- set to 1 if output=0
+
+port(
+	A:in std_logic_vector(15 downto 0);
+	B:in std_logic_vector(15 downto 0);
+	sel:in std_logic_vector(1 downto 0);
+	
+	Z:out std_logic_vector(15 downto 0)
+);
+
 end ALU_16_bit;
 
-architecture ALU of ALU_16_bit is
+architecture Behavioural of ALU_16_bit is
+
 begin
-
+	
 	process(A,B,sel)
-		begin
-			case sel is 
-			when "00" 	=> null;
-			when "01" 	=> null;
-			when "10" 	=> op <= A NAND B;
-			when "11" 	=> op <= A XOR  B;
-			when others => null;
-			end case;
+	begin
+	
+		case sel is
+			
+			when "00" =>
+				Z<=A nand B;
+			
+			when "01" =>
+				Z<=A nand B;
+				
+			when "10" =>
+				Z<=A nand B;
+				
+			when "11" =>
+				Z<=A xor B;
+				
+		end case;
 		
-		if (op = 0) then											-- setting z flag to 1 if output is 0
-		z <= '1';
-		else
-		z <='0';
-		end if;
-		end process;
+	end process;
+	
+end Behavioural;
 
-end ALU;
