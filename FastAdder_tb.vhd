2482ba802f050b2a7de258643612f3c24c30a9c9
Nirmal shah <62292293+nirmalshah123@users.noreply.github.com>
2020-12-01 13:54:51 +0530

    Update FastAdder_tb.vhd

--- FastAdder_tb.vhd
+++ FastAdder_tb.vhd
@@ -1,48 +1,39 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.numeric_std.ALL;
 entity FastAdder_tb is
 end FastAdder_tb;
 
 architecture tb of FastAdder_tb is
 
-	signal x, y : std_logic_vector(15 downto 0); -- inputs
-	signal o: std_logic_vector(15 downto 0);
-	signal oCout: std_logic_vector(15 downto 0);	-- outputs
+	signal x, y : std_logic_vector(15 downto 0);			 -- inputs
+	signal o: std_logic_vector(15 downto 0);				 -- output
+	signal oC: std_logic_vector(15 downto 0);										 -- Output carry
 	
 	
 	component FastAdder  is
-	port(A, B: in std_logic_vector(15 downto 0);
-	Z,ZCout: out std_logic_vector(15 downto 0));
+	port(
+	A		:in std_logic_vector(15 downto 0);				--Input A
+	B		:in std_logic_vector(15 downto 0);				--Input B
+	Carry	:out std_logic_vector(15 downto 0);										--Final carry
+	op		:out std_logic_vector(15 downto 0));			--Output		
 	end component;
 	begin
 
 
 dut_instance: FastAdder
 
-	port map (A => x, B => y, Z => o, ZCout=> oCout);
+	port map (A => x, B => y, Carry => oC, op=> o);		--mapping
 	
 	process-- inputs
 	
 	begin
-
-	for m in 0 to 255 loop 
-		
-		
-		x <= std_logic_vector(to_SIGNED(m,16)); 
-		
-			for n in 0 to 255 loop 
-			
-				y <= std_logic_vector(to_SIGNED(n,16)); 
-				
-				wait for 1 ns; 
-				
-				assert (to_integer(SIGNED(o)) = (m + n)) 
-				report "Incorrect sum" severity failure;
-				
-				
-			end loop; 
-		end loop; 
+	x <= "0111111111111111";
+	y <= "0111111111111111";
+	
+	wait for 10 ns;
+	
+	
 	
 	end process;
+	
 	end tb ;
