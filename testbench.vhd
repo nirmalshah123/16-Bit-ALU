44b1a78cb5d3b3877b8540f08b53447c818ab230
Nirmal shah <62292293+nirmalshah123@users.noreply.github.com>
2020-12-02 01:02:13 +0530

    Update testbench.vhd

--- testbench.vhd
+++ testbench.vhd
@@ -7,29 +7,30 @@ end testbench;
 
 architecture tb of testbench is
 
-signal A,B 	: signed(15 downto 0);
+signal A,B 	: std_logic_vector(15 downto 0);
 signal sel	: std_logic_vector(1 downto 0);
-signal op 	: signed(15 downto 0);
 signal c		: std_logic;
+signal op 	: std_logic_vector(15 downto 0);
 signal z 	: std_logic;
 
 component ALU_16_bit is 
-port (A,B 	: in signed(15 downto 0);						-- A,B are inputs
-		sel	: in std_logic_vector(1 downto 0);			-- control pins for MUX
-		op 	: buffer signed(15 downto 0);					-- 16 bit output
-		c		: out std_logic;									-- carry
-		z 		: out std_logic);									-- set to 1 if output=0
+port (A,B 	: in std_logic_vector(15 downto 0);						-- A,B are inputs
+		sel	: in std_logic_vector(1 downto 0);						-- control pins for MUX
+		Carry_final : out std_logic := '0';								-- Carry
+		op 	: buffer std_logic_vector(15 downto 0);				-- 16 bit output
+		z 		: out std_logic := '0');									-- set to 1 if output=0
 end component;
 
 begin
 
 dut_instance : ALU_16_bit
-port map(A =>A,B=>B,sel=>sel,op=>op,c=>c,z=>z);
+port map(A =>A,B=>B,sel=>sel,Carry_final => c,op=>op,z=>z);
 process
 begin
-A <="1010101011111111";
-B <="1010101110011111";
-sel<="10";
+A <="1111111111111111";
+B <="1111111111111111";
+sel<="01";
 wait for 5ns;
+
 end process;
 end tb;
